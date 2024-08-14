2e07f5d0371c80d4979843c6b31bbf1f1d4ebb48
Yezi99999 <Zeroixc@outlook.com>
2024-08-14 20:34:11 +0800

    have some test

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+const message = ref('Hellow Vue!')
+
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <HelloWorld/>
+</template>
+
