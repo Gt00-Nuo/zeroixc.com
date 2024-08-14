dcad7f7f2810fef965850945585fc2bbb53e91a3
Yezi99999 <Zeroixc@outlook.com>
2024-08-15 01:44:25 +0800

    some change

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ const message = ref('Hellow Vue!')
 
 <template>
   <h1>{{ message }}</h1>
+  <p>学习vue中……<br>此后作为一个web端的交互页面</p>
   <HelloWorld/>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,38 @@
-<template>
-    <div :class="msg">test</div>
-</template>
-  
-<script>
-export default{
-    data(){
-        return{
-            msg:"active"
-        }
-    }
+<script setup>
+import { ref } from 'vue';
+
+// export default{
+//     data(){
+//         return{
+//             dynameClass:"appclass",
+//             dynameId:"appid",
+//             isButtonDisabled:false,
+//             objectOfAttres:{
+//                 class:"appclass",
+//                 id:"appid"
+//             }
+//         }
+//     }
+// }
+const awesome = ref(false)
+const dynameId="appid"
+const dynameClass="appclass"
+
+function toggle() {
+    awesome.value = !awesome.value
 }
+
 </script>
+<template>
+    <div :id="dynameId" :class="dynameClass" v-if=awesome>objectOfAttres</div>
+    <div :id="dynameId" :class="dynameClass" v-else>test</div>
+    <button v-on:click="toggle">click</button>
+</template>
+
+<style>
+
+.appclass{
+    color: #ec261b;
+    font-size: 30px;
+}
+</style>
